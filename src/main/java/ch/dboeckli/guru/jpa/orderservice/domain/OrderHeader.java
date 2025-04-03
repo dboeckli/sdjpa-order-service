a7e401fc51f631b8d21f212718f22c905c956bae
adm-boeckli <dboeckli@gmail.com>
2025-04-03 15:59:38 +0200

    add embedded type

--- src/main/java/ch/dboeckli/guru/jpa/orderservice/domain/OrderHeader.java
+++ src/main/java/ch/dboeckli/guru/jpa/orderservice/domain/OrderHeader.java
@@ -1,9 +1,6 @@
 package ch.dboeckli.guru.jpa.orderservice.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,29 +11,69 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class OrderHeader {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@AttributeOverrides({
+    @AttributeOverride(
+        name = "shippingAddress.address",
+        column = @Column(name = "shipping_address")
+    ),
+    @AttributeOverride(
+        name = "shippingAddress.city",
+        column = @Column(name = "shipping_city")
+    ),
+    @AttributeOverride(
+        name = "shippingAddress.state",
+        column = @Column(name = "shipping_state")
+    ),
+    @AttributeOverride(
+        name = "shippingAddress.zipCode",
+        column = @Column(name = "shipping_zip_code")
+    ),
+    @AttributeOverride(
+        name = "billToAddress.address",
+        column = @Column(name = "bill_to_address")
+    ),
+    @AttributeOverride(
+        name = "billToAddress.city",
+        column = @Column(name = "bill_to_city")
+    ),
+    @AttributeOverride(
+        name = "billToAddress.state",
+        column = @Column(name = "bill_to_state")
+    ),
+    @AttributeOverride(
+        name = "billToAddress.zipCode",
+        column = @Column(name = "bill_to_zip_code")
+    )
+})
+public class OrderHeader extends BaseEntity  {
 
     private String customer;
+
+    @Embedded
+    private Address shippingAddress;
+
+    @Embedded
+    private Address billToAddress;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof OrderHeader that)) return false;
+        if (!super.equals(o)) return false;
 
-        OrderHeader that = (OrderHeader) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        return customer != null ? customer.equals(that.customer) : that.customer == null;
+        if (getCustomer() != null ? !getCustomer().equals(that.getCustomer()) : that.getCustomer() != null)
+            return false;
+        if (shippingAddress != null ? !shippingAddress.equals(that.shippingAddress) : that.shippingAddress != null)
+            return false;
+        return billToAddress != null ? billToAddress.equals(that.billToAddress) : that.billToAddress == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        int result = super.hashCode();
+        result = 31 * result + (getCustomer() != null ? getCustomer().hashCode() : 0);
+        result = 31 * result + (shippingAddress != null ? shippingAddress.hashCode() : 0);
+        result = 31 * result + (billToAddress != null ? billToAddress.hashCode() : 0);
         return result;
     }
 }
--- src/main/resources/h2-schema.sql
+++ src/main/resources/h2-schema.sql
@@ -2,6 +2,14 @@ drop table if exists order_header cascade;
 
 create table order_header
 (
-    id       bigint not null auto_increment primary key,
-    customer varchar(255)
+    id                  bigint not null auto_increment primary key,
+    customer            varchar(255),
+    shipping_address    varchar(30),
+    shipping_city       varchar(30),
+    shipping_state      varchar(30),
+    shipping_zip_code   varchar(30),
+    bill_to_address     varchar(30),
+    bill_to_city        varchar(30),
+    bill_to_state       varchar(30),
+    bill_to_zip_code    varchar(30)
 );
