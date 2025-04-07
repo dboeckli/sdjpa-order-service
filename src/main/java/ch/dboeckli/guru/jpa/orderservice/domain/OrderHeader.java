fcda59eb46efe6b112944a1b221b1b3160348444
adm-boeckli <dboeckli@gmail.com>
2025-04-07 14:18:31 +0200

    implement bidirectional one to one

--- src/main/java/ch/dboeckli/guru/jpa/orderservice/domain/OrderHeader.java
+++ src/main/java/ch/dboeckli/guru/jpa/orderservice/domain/OrderHeader.java
@@ -66,8 +66,13 @@ public class OrderHeader extends BaseEntity {
     @ToString.Exclude
     private Set<OrderLine> orderLines;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, mappedBy = "orderHeader")
     private OrderApproval orderApproval;
+
+    public void setOrderApproval(OrderApproval orderApproval) {
+        this.orderApproval = orderApproval;
+        orderApproval.setOrderHeader(this);
+    }
 
     public void addOrderLine(OrderLine orderLine) {
         if (orderLines == null) {
