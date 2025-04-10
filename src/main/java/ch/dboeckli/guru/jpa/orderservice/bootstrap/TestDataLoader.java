a4b869643aeab1a5db7b480fd7e5dc8a38834825
Dominique Boeckli <dboeckli@gmail.com>
2025-04-10 12:07:53 +0200

    add tests for transactions (#13)
    * add tests for transactions
    
    * add tests for lazy loading

--- src/main/java/ch/dboeckli/guru/jpa/orderservice/bootstrap/TestDataLoader.java
+++ src/main/java/ch/dboeckli/guru/jpa/orderservice/bootstrap/TestDataLoader.java
@@ -1,4 +1,4 @@
-package ch.dboeckli.guru.jpa.orderservice.loader;
+package ch.dboeckli.guru.jpa.orderservice.bootstrap;
 
 import ch.dboeckli.guru.jpa.orderservice.domain.*;
 import ch.dboeckli.guru.jpa.orderservice.repository.CustomerRepository;
@@ -6,6 +6,7 @@ import ch.dboeckli.guru.jpa.orderservice.repository.OrderHeaderRepository;
 import ch.dboeckli.guru.jpa.orderservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.LazyInitializationException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,8 @@ import java.util.Random;
 @RequiredArgsConstructor
 @Slf4j
 public class TestDataLoader implements CommandLineRunner {
+
+    public static final String CUSTOMER_NAME_DEMO = "Customer For Single Order Demo";
 
     final String PRODUCT_D1 = "Product 1";
     final String PRODUCT_D2 = "Product 2";
@@ -32,8 +35,23 @@ public class TestDataLoader implements CommandLineRunner {
 
     private final ProductRepository productRepository;
 
+    private final BootstrapOrderService bootstrapOrderService;
+
     @Override
     public void run(String... args) {
+        OrderHeader singleOrderHeader = createSingleOrderHeader();
+
+        /*
+        demostation of following fact: https://docs.spring.io/spring-framework/reference/data-access/transaction/declarative/annotations.html
+        Method visibility and @Transactional in proxy mode
+
+        The @Transactional annotation is typically used on methods with public visibility. As of 6.0, protected or package-visible methods can also be made transactional for class-based proxies by default.
+        Note that transactional methods in interface-based proxies must always be public and defined in the proxied interface. For both kinds of proxies, only external method calls coming in through the proxy
+        are intercepted.
+         */
+        demonstrateLazyLoading(singleOrderHeader.getId()); // Demonstrate lazy loading with LazyInitializationException
+        bootstrapOrderService.demonstrateLazyLoading(singleOrderHeader.getId()); // Demonstrate lazy loading without LazyInitializationException
+
         log.info("### Loading test data...");
         List<Product> products = loadProducts();
         Customer customer = loadCustomers();
@@ -45,6 +63,49 @@ public class TestDataLoader implements CommandLineRunner {
 
         orderHeaderRepository.flush();
         log.info("### Test data loaded successfully! {} orders created.", ORDERS_TO_CREATE);
+    }
+
+    private OrderHeader createSingleOrderHeader() {
+        OrderHeader orderHeader = new OrderHeader();
+
+        Customer customer = new Customer();
+        customer.setCustomerName(CUSTOMER_NAME_DEMO);
+        Customer savedCustomer = customerRepository.save(customer);
+
+        orderHeader.setCustomer(savedCustomer);
+
+        Product newProduct = new Product();
+        newProduct.setProductStatus(ProductStatus.NEW);
+        newProduct.setDescription("Product For Single Order  Demo");
+        Product product = productRepository.saveAndFlush(newProduct);
+
+        OrderLine orderLine = new OrderLine();
+        orderLine.setQuantityOrdered(5);
+        orderLine.setProduct(product);
+
+        orderHeader.addOrderLine(orderLine);
+
+        OrderApproval approval = new OrderApproval();
+        approval.setApprovedBy("me");
+        orderHeader.setOrderApproval(approval);
+
+        OrderHeader savedOrder = orderHeaderRepository.save(orderHeader);
+        orderHeaderRepository.flush();
+        return savedOrder;
+    }
+
+    private void demonstrateLazyLoading(Long singleOrderHeaderId) {
+        OrderHeader orderHeader = orderHeaderRepository.findById(singleOrderHeaderId).orElse(null);
+        orderHeader.getOrderLines().forEach(orderLine -> {
+            log.info("### Product Description: {}", orderLine.getProduct().getDescription());
+
+            // here we get a lazy loading initializing exception
+            try {
+                orderLine.getProduct().getCategories().forEach(category -> log.info("### Category: {}", category.getDescription()));
+            } catch (LazyInitializationException ex) {
+                log.error("### Expected LazyInitializationException for demonstration", ex);
+            }
+        });
     }
 
     private void saveOrder(Customer customer, List<Product> products){
