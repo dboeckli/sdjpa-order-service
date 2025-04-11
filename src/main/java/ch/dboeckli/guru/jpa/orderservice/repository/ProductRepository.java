6fcd978dbaf6b0c88cd5967f8c61db0bb603856f
adm-boeckli <dboeckli@gmail.com>
2025-04-11 11:14:41 +0200

    add test for quantity_on_hand and pessimistic locking

--- src/main/java/ch/dboeckli/guru/jpa/orderservice/repository/ProductRepository.java
+++ src/main/java/ch/dboeckli/guru/jpa/orderservice/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package ch.dboeckli.guru.jpa.orderservice.repository;
 
 import ch.dboeckli.guru.jpa.orderservice.domain.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
@@ -9,4 +12,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>  {
 
     Optional<Product> findByDescription(String description);
 
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @NonNull
+    Optional<Product> findById(@NonNull Long id);
 }
